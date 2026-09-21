4ef40558343adaa0a42c3637e6c18ca31de0192c
ten848 <tianshand914@gmail.com>
2026-09-21 21:52:15 +0900

    O

--- pages/2_sub.py
+++ pages/2_sub.py
@@ -245,6 +245,6 @@ def status_sum():
         耐久_sum [team] += 耐久_df[z]
 
 status_sum()
-st.write(DPS_sum[0], 耐久_sum[0])
-st.write(DPS_sum[1], 耐久_sum[1])
+st.write(DPS_sum[0], DPS_sum[0])
+st.write(耐久_sum[0], 耐久_sum[1])
 
